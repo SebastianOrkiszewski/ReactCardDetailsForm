93f39b0386e66dd4dd2cdf81320269dcc8e20ec6
SebastianOrkiszewski <s.orkiszewski@gmail.com>
2022-11-15 16:53:36 +0100

    Add success component

---
+++ src/components/Success/Success.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "assets/images/icon-complete.svg"
+import { Wrapper, Button } from "views/Success/Success.styles";
+import { Link } from 'react-router-dom';
+
+const Success = () => {
+
+ 
+
+  return (
+    <Wrapper>
+      <img alt="ok" src={logo} />
+      <h1>THANK YOU!</h1>
+      <p>We've added you card details</p>
+      <Link to="/"><Button type="reset" >Continue</Button></Link>
+    </Wrapper>
+  );
+};
+
+export default Success;
