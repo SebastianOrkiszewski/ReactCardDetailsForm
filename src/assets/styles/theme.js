cf558ce5a52079323a2c8a74556bc469f34ccda4
SebastianOrkiszewski <s.orkiszewski@gmail.com>
2022-11-15 16:31:09 +0100

    Add Global Styles and images

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+      activeInput: 'hsl(249, 99%, 64%)',
+      letters: 'hsl(278, 94%, 30%)',
+      inputError: 'hsl(0, 100%, 66%)',
+      white: 'hsl(0, 0%, 100%)',
+      lightGray: 'hsl(270, 3%, 87%)',
+      darkGray: 'hsl(279, 6%, 55%)',
+      black: '#000000',
+      
+    },
+    fontSize: {
+      standard: '18px',
+    },
+  };
